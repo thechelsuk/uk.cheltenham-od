690cc5905796d75229e41974ef04bcf7fdba9d23
thechelsuk <GitHub@thechels.uk>
2026-09-14 11:56:17 +0100

    tidying up
    Signed-off-by: thechelsuk <GitHub@thechels.uk>

--- _python/local/fetch-fuel-pricing.py
+++ _python/local/fetch-fuel-pricing.py
@@ -6,7 +6,7 @@ import datetime
 import time
 import sys
 import requests
-
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 import helper
 
 # Load .env file for local development if present
